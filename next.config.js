98b63dfc18ba3f4b1ad58bc853119a7abecd8f33
justin1508 <justin1508@hotmail.com>
2023-01-04 08:35:39 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  experimental: {
+    images: {
+      allowFutureImages: true,
+    }
+  }
 }
 
 module.exports = nextConfig
---
+++ src/pages/index.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
+
+import camiseta1 from '../assets/1.png'
+import camiseta2 from '../assets/2.png'
+import camiseta3 from '../assets/3.png'
+
+export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
+
+  return (
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={camiseta1} width={520} height={480} alt=""/>
+
+        <footer>
+          <strong>Camisa 1</strong>
+          <span>R$ 89,90</span>
+        </footer>
+      </Product>
+
+      <Product  className="keen-slider__slide">
+        <Image src={camiseta2} width={520} height={480} alt=""/>
+
+        <footer>
+          <strong>Camisa 1</strong>
+          <span>R$ 89,90</span>
+        </footer>
+      </Product>
+
+      <Product  className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt=""/>
+
+        <footer>
+          <strong>Camisa 1</strong>
+          <span>R$ 89,90</span>
+        </footer>
+      </Product>
+
+      <Product  className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt=""/>
+
+        <footer>
+          <strong>Camisa 1</strong>
+          <span>R$ 89,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+      )
+}
